e4b497cb284f5c60c8be81a1413b24fcc6fe7189
augisdh <34551243+augisdh@users.noreply.github.com>
2018-04-18 18:12:49 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const secondsHand = document.querySelector(".sec");
+const minutesHand = document.querySelector(".min");
+const hoursHand = document.querySelector(".hour");
+
+function setDate(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    if(secondsDegrees === 90){
+        secondsHand.style.transition = "none";
+    } else {
+        secondsHand.style.transition = "all 0.2s";
+    }
+
+    if(minutesDegrees === 90){
+        minutesHand.style.transition = "none";
+    } else {
+        minutesHand.style.transition = "all 0.2s";
+    }
+}
+
+setInterval(setDate, 1000);
