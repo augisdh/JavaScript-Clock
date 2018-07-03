7c5eef5dc89905823b9b2f7e1f0030dd153577f9
Augustas <34551243+augisdh@users.noreply.github.com>
2018-07-03 20:30:31 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ const hoursHand = document.querySelector(".hour");
 
 function setDate(){
     const now = new Date();
-
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
